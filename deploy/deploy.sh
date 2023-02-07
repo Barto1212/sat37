52dead249c44ba97e56ba05cd93d7b236c7281e2
Barto1212 <loic_barthomeuf@hotmail.fr>
2023-02-07 22:57:11 +0100

    feat(deploy)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-user=$1
-ipAdress=$2
-localadressScript="/var/www/loic-barthomeuf"
-localadressSources=$localadressScript"/sources"
+adressServer=$1
+
 
 adressComputer="$user""@""$ipAdress"
 
@@ -11,25 +9,28 @@ adressSources=$adressComputer":"$localadressSources
 
 # copie de la source front
 cd ..
-tar --exclude="node_modules" --exclude="package-lock.json" --exclude=".next" -cf sources.tar *
-
-gzip sources.tar
+# tar --exclude="node_modules" --exclude="package-lock.json" --exclude=".next" -cf sources.tar *
+tar -czf sources.tar.gzip out
 
 
 # copie du dossier compressé
-scp sources.tar.gz $adressScript
+scp sources.tar.gzip $adressServer":/tmp"
+ssh $adressServer rm -R /var/www/html/loic-barthomeuf/*
+ssh $adressServer tar -xzf /tmp/sources.tar.gzip -C /tmp/
+ssh $adressServer cp -R /tmp/out/* /var/www/html/loic-barthomeuf
+ssh $adressServer rm -R /tmp/out
 
-cd ./deploy
+# cd ./deploy
 # copie du script à éxécuter
-scp config/apideployserver.sh $adressScript
+# scp config/apideployserver.sh $adressScript
 
 # on le convertit :
-ssh $adressComputer dos2unix $localadressScript"/"apideployserver.sh
+# ssh $adressComputer dos2unix $localadressScript"/"apideployserver.sh
 # on exécute le script
-ssh $adressComputer $localadressScript"/"apideployserver.sh
+# ssh $adressComputer $localadressScript"/"apideployserver.sh
 # on le supprime :
-ssh $adressComputer rm $localadressScript"/"apideployserver.sh
+# ssh $adressComputer rm $localadressScript"/"apideployserver.sh
 
-cd ..
-
-rm sources.tar.gz
+# cd ..
+ssh $adressServer rm /tmp/sources.tar.gzip
+rm sources.tar.gzip
